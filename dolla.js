cdd50b64ebac67f99d5f11aad4907b5472111245
Juan Pablo Canepa <jpcanepa@gmail.com>
2017-07-10 12:17:00 -0400

    Changed confirmation message

--- dolla.js
+++ dolla.js
@@ -4,13 +4,24 @@ var controller = Botkit.slackbot({send_via_rtm: true});
 
 var bot = controller.spawn({token : process.env.SLACK_BOT_TOKEN});
 
+function unixNow()
+{
+    return Date.now() / 1000|0;
+}
+
 controller.hears([/Hagamos un ([^ ?]*)\s*\??/],['mention','direct_mention','ambient'], function(bot, message) {
     let event_name = message.match[1];  
     bot.startConversation(message, function(err, convo){
 
         convo.setVar('event_name', event_name);
         
-        convo.addMessage('[{{vars.event_name}}] ¡La raja!', 'yes_thread');
+        convo.addMessage('¡La raja!', 'yes_thread');
+        convo.addMessage(
+            {
+                mrkdwn: true,
+                text: `**{{vars.event_name}} <!date^${unixNow()}^{date_long}|hoy>**`
+            }, 'yes_thread');
+
         convo.addMessage({action:'completed', text:'[{{vars.event_name}}] Bu. Será para la otra'}, 'no_thread');
 
 
